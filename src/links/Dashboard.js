68b9c22c321e284ef6b1bddbc3ddd24a4bc2ae72
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2023-03-08 23:41:47 +0530

    Revert "Merge branch 'main' of https://github.com/typeerror101/better_help"
    This reverts commit 2940ae1d37867f2c354d4ac61a01503459114100, reversing
    changes made to 62e2f2c81e7c39d8ea97032c0d3a1d08f05d8561.

--- src/links/Dashboard.js
+++ src/links/Dashboard.js
@@ -7,7 +7,6 @@ import { useNavigate } from "react-router-dom";
 
 import { Button } from '@mui/material';
 import { IconBase } from 'react-icons';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
 
 export default function Dashboard() {
@@ -18,7 +17,7 @@ export default function Dashboard() {
     // if(user) navigate("/")
     if(!user) navigate("/login");
   return (
-    <div className="Mainbox container m-5 p-3 bg-slate-200 rounded-3xl">
+    <div className="container m-5 p-3 bg-slate-200 rounded-3xl">
       <div className='w-auto flex flex-wrap px-5 py-5 m-7 justify-between' >
         <div className=" m-3 p-2 rounded-xl" > 
           <h1 className>Good Morning, {user.displayName}!</h1>
@@ -28,41 +27,15 @@ export default function Dashboard() {
           <div>
            Talk to you Therapist now
           </div>
-          <Button variant="contained">Start</Button>
+          <Button variant="contained">Contained</Button>
         </div>
       </div>
-      <div className='secondContainer flex flex-wrap w-full bg-slate-300 rounded-2xl p-5'>
-        <div className='leftHalf w-2/3 gap-y-10'>
-          <div className="searchBar w-full flex flex-wrap gap-2">
-            <div className="bg-slate-400 w-4/5 rounded-xl">
-              Search
-            </div>
-            <FilterAltIcon className='bg-slate-400 '/>
+      <div className='container  bg-slate-300 rounded-2xl p-5'>
+        <div class="w-2/3">
+          <div className="bg-slate-400 rounded-xl">
+            Search
           </div>
-          <div className='compilation justify-start p-5'>
-            <h1>Today's compilation for you</h1>
-            <div className='container flex flex-wrap gap-4'>
-              <div className='card1 w-2/6 h-40 bg-amber-50 rounded-3xl'>
-              </div>
-              <div className='card2 w-1/6 h-40 bg-amber-50 rounded-3xl'>
-              </div>
-              <div className='card3 w-2/6 h-40 bg-amber-50 rounded-3xl'>
-              </div>
-            </div>
-          </div>
-          <div className='activities justify-start p-5'>
-            <h1>A selection of activities and music for you</h1>
-            <div className='container flex flex-wrap gap-4'>
-              <div className='card1 w-1/6 h-40 bg-amber-50 rounded-3xl'>
-              </div>
-              <div className='card2 w-2/6 h-40 bg-amber-50 rounded-3xl'>
-              </div>
-              <div className='card3 w-2/6 h-40 bg-amber-50 rounded-3xl'>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='rightHalf h-100% w-1/3 bg-yellow-300 rounded-3xl'>
+          <IconBase></IconBase>
         </div>
       </div>
       <br /><br /><br /><br />
