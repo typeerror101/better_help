e65a165b964a344b41bf8c301df63544d62ff210
Muhammad Danish <danishpk175@gmail.com>
2023-05-17 14:00:30 +0530

    blog

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Blog from './links/Blog';
+import Blog1 from './links/Blog1';
+import Blog2 from './links/Blog2';
 import Home from './links/Home';
 import Login from './links/Login';
 import Dashboard from './links/Dashboard';
@@ -9,28 +11,30 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
 
-  
+
   // if(user) navigate("/");
 
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className='components'>
-          <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/blog' element={<Blog/>}/>
-              <Route path='/about'/>
-              <Route path='/depressionTest' element={<DepressionTest/>}/>
-              <Route path='/faq'/>
-              <Route path='/parent' element={<Parent/>}/>
-              <Route path='/therapistjobs'/>
-              <Route path='/support' element={<Dashboard/>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/getstarted'/>
-              <Route path='/dashboard' element={<Dashboard/>}/>
-          </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path="/blog1" element={<Blog1 />} />
+          <Route path='/blog2' element={<Blog2/>}></Route>
+          <Route path='/about' />
+          <Route path='/depressionTest' element={<DepressionTest />} />
+          <Route path='/faq' />
+          <Route path='/parent' element={<Parent />} />
+          <Route path='/therapistjobs' />
+          <Route path='/support' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/getstarted' />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
       </div>
-      
+
     </>
   );
 }
--- src/links/Blog.js
+++ src/links/Blog.js
@@ -1,13 +1,169 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "../styles/blog.css";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import SearchIconWrapper from '@mui/icons-material/Search';
+import StyledInputBase from '@mui/material/InputBase';
 
-function blog() {
+
+
+export default function ImgMediaCard() {
+
+
   return (
-    <div>
-      <h1>This is the custom Blog Page.</h1>
-      <h2>Currently work in progress....</h2>
-      
-    </div>
-  )
-}
+    <>
+      <div className='heading'>
+        <h1>Mental Health Blog</h1>
+      </div>
 
-export default blog
+      <div className='header'>
+          <FormControl sx={{ m: 1, minWidth: 120, backgroundColor: '' }} className="select-container">
+            <InputLabel htmlFor="grouped-native-select">Categories</InputLabel>
+            <Select native defaultValue="" id="grouped-native-select" label="Grouping">
+              {/* <option aria-label="None" value="" /> */}
+              <optgroup>
+                <option value={1}>Active Duty</option>
+                <option value={2}>Anxiety Disorders</option>
+                <option value={2}>Personality Disorders</option>
+                <option value={2}>Treatment Resistant Depression</option>
+              </optgroup>
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ m: 1, minWidth: 120 }} className="select-container">
+            <InputLabel htmlFor="grouped-select">Archives</InputLabel>
+            <Select defaultValue="" id="grouped-select" label="Grouping">
+              <MenuItem value=""></MenuItem>
+              <MenuItem value={1}>January</MenuItem>
+              <MenuItem value={2}>February</MenuItem>
+              <MenuItem value={2}>March</MenuItem>
+              <MenuItem value={2}>April</MenuItem>
+              <MenuItem value={2}>May</MenuItem>
+              <MenuItem value={2}>June</MenuItem>
+              <MenuItem value={2}>July</MenuItem>
+              <MenuItem value={2}>August</MenuItem>
+              <MenuItem value={2}>September</MenuItem>
+              <MenuItem value={2}>October</MenuItem>
+              <MenuItem value={2}>November</MenuItem>
+              <MenuItem value={2}>December</MenuItem>
+            </Select>
+          </FormControl>
+  
+          <div className="search-input-container">
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              style={{ backgroundColor: 'white' }}
+            />
+          </div>  
+        </div>
+
+
+
+      <div className='Cards'>
+
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/images/img1.png"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              The Comforting and Frustrating Realization that You Are Not Alone
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              However frustrating it was to hear that others struggle with similar challenges, I now know that frustration points to a more important truth.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to="/Blog1">
+              <Button size="small">Learn More</Button>
+            </Link>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/images/img2.png"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Early Psychosis Intervention
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              The early identification and treatment of psychosis is possible.
+            </Typography>
+          </CardContent>
+          <CardActions>
+          <Link to="/Blog2">
+              <Button size="small">Learn More 1</Button>
+            </Link>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/images/img3.png"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              I Wish They’d Seen It Sooner: Growing Up Undiagnosed
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Healthy Coping Mechanisms
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Improved Self-Concept
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Emotional Stability
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/images/img4.png"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Navigating Early Recovery from Schizophrenia
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </div>
+    </>
+  );
+}
