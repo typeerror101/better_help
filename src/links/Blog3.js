6243c237913708c9c4b76e6830067b6a2e76b94a
Muhammad Danish <danishpk175@gmail.com>
2023-05-17 23:56:30 +0530

    improved version

---
+++ src/links/Blog3.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import "../styles/blog.css";
+
+
+export default function () {
+  return (
+    <div>
+        <h1 className='blog1'>I Wish They’d Seen It Sooner: Growing Up Undiagnosed</h1>
+
+        <p className='description'>“Kelly, come to the office please.”
+
+My heart raced as the classroom intercom cut out. Up until then, I’d been half-listening to a lecture in my fifth-grade social studies class, but now I was wide awake. Did I do something wrong? Am I in trouble? Countless possibilities flooded my mind as I speed-walked through the school hallways. Finally, I turned the corner to reception. Standing there — hands on hips, smirking from ear-to-ear — was my dad. A wave of relief swept over me but was quickly replaced with confusion.
+
+“What are you doing here?” I asked.
+
+He laughed. “You forgot your lunch… again.”
+
+The school secretary let out a chuckle. This wasn’t her first time meeting either of my parents. They were regulars. Seemingly every week, sometimes more, I’d leave behind an item at home. It didn’t matter if it was my lunch, my homework, even my entire backpack — I was always forgetting something.
+
+I sighed in embarrassment. “Thank you, Dad.”
+
+He turned to the secretary. “That’s my Chrissy Snow.” Indeed, my parents nicknamed their 10-year-old daughter after the infamously air-headed “Three’s Company” character (It didn’t help that my hair was blonde, too).
+
+Stories like this plagued my childhood. I was a smart student — always top of my class — but when it came to memory, attentiveness or and any concept of time, I was hopelessly lost.
+
+I didn’t know it at the time, but I was struggling with more than just a bout of forgetfulness.
+
+Getting An ADHD Diagnosis
+It wasn’t until I was 19 years old that I was diagnosed with attention deficit hyperactivity disorder (ADHD). Initially, I was shocked. My perception of the disorder was based on its stereotype: hyped-up little boys, acting out recklessly in class. I couldn’t believe it. But that’s because I wasn’t the poster child for ADHD.
+
+The way my therapist explained it, not many people are. ADHD varies from person to person, based on both gender and each of the disorder’s specific subtypes (inattentive, hyperactive or combination). It began to make sense why I, an adult woman, looked nothing like the typical hyperactive schoolboy. I was floored.
+
+When I think back to my childhood and my difficult teen years, I often wondered if having this information earlier could have changed my mental health. After all, this wasn’t the only diagnosis I was facing. For years, I’d also lived in the dark with anxiety and type two bipolar disorder.
+
+One part of me was always afraid — my anxiety could turn any situation into cause for concern. Am I safe here? What if I failed my test? Do my friends actually like me? These thoughts were incessant. And without treatment, they became my inner voice, relentlessly expecting the worst in every encounter.
+
+Another part of me was powerless, tethered to some invisible force that could either lift or sink my mood. Unexplainable episodes (what I now know to be bipolar depression/hypomania) would suddenly take over. Weeks and months of losing my will to live would quickly boomerang into spurts of restless energy and creativity. Inevitably, I’d crash, and the cycle would repeat itself. It was exhausting not having an answer.
+
+Knowing about my ADHD earlier could have been a real game changer in terms of spotting and treating these other disorders. Access to a psychiatrist or therapist alone could have saved me years of unanswered questions about why I was always so worried, or what was causing me to spiral into extreme moods. My diagnosis did illuminate a fuller picture of my mental health. It’s the reason I can better take care of my anxiety, and it brought me one step closer to finding out about my bipolar disorder. But it does make me sad to think some of my suffering could have been alleviated sooner.
+
+Grieving The Girl I Could Have Been
+With my ADHD diagnosis came several stages. Surprise turned to relief, and relief turned to mourning. How had no one seen this sooner? I thought back to all those years I struggled. The blame I placed on myself, the near constant negative self-talk, the highs and lows could all have been addressed had someone recognized the signs for what they were. I’d lost precious time and gained countless insecurities.
+
+Sure, I was successful. Every year, teachers came to my parents with glowing reviews. I was acing my exams, participating in class and helping other students when I could. But at what cost? What about the late nights I’d pull, crying and spending hours on assignments that should have taken 30 minutes? Or the panic attacks I’d have over papers I’d waited until the last second to write?
+
+My social life suffered in the same way. A consistent daydreamer, I almost always found myself zoning in and out of conversations. My friends would laugh about it — and so would I — but sometimes I couldn’t help feeling like the butt of the joke. Other times, I felt so overstimulated (sensory issues are common with ADHD) by noise, crowds or by fear of rejection that I’d leave hangouts early, missing out on time and inside jokes with the people I cared about most.
+
+To this day, I think about the self-esteem I could have had and the emotional well-being I could have enjoyed. The resolve to work. The capacity to tolerate discomfort. These things could have come to fruition had my ADHD diagnosis and treatment come sooner. I think I’d be punctual. Level-headed. Much more confident. I’d be a young woman better equipped to cope with daily life. My mental health would have been different. I’m proud of the person I am today, and I have developed some of these qualities, but it pains me to think of the possibilities I missed growing up.
+
+The Importance Of Early Intervention
+Living in the dark for as many years as I did, and struggling with my mental health for most of my childhood, taught me the supreme significance of finding out about and treating ADHD as early as possible. Only now do I have the information and tools I need to succeed (therapy, medication, school accommodations and more). Even so, I have a long process of trial-and-error ahead. So, in my experience, these are a few benefits of early intervention that could have made a difference in my life and anyone else’s whose ADHD went undetected:
+
+Healthy Coping Mechanisms
+Prompt action by parents, teachers and therapists encourages healthier behaviors from a much younger age, preventing unhealthy practices like procrastination, avoidance, dangerous decision-making, substance use, etc.
+
+Improved Self-Concept
+Being equipped with foundational coping skills undoubtedly bolsters self-esteem. Having and taking such steps toward success could lead to more sustainable academic achievements, better social interactions and more secure relationships.
+
+Emotional Stability
+Earlier treatment can repel many negative feelings, resulting in less chaos, fewer real and perceived failures — and an overall better quality of life. And according to a 2019 study, protective measures against ADHD have even been shown to lessen the likelihood of developing several common mental illnesses, including the other two diagnoses I have.
+
+It wasn’t easy growing up with undiagnosed ADHD. Had access to help been available to me sooner, I may have encountered fewer social, emotional and mental struggles. This problem isn’t unique to me either. There are many individuals who face this disorder alone because of a lack of adequate mental health education. How can we identify a disorder when we only envision an often-misguided stereotype? But that’s not to say hope isn’t on the horizon. Stigmas surrounding ADHD and other illnesses are fading, so with time and concerted societal effort, I do believe change is on the way.</p>
+    </div>
+  )
+}
---
+++ src/links/Blog4.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "../styles/blog.css";
+
+
+export default function () {
+  return (
+    <div>
+        <h1 className='blog1'>Navigating Early Recovery from Schizophrenia</h1>
+
+        <p className='description'>Sometime between March 8 and March 15 of this year I was diagnosed with schizophrenia. I was at the psych ward when my doctor looked me in the eyes and said, “Maya, you will be ok.” Despite hearing the doctor’s diagnosis, I still couldn’t believe that I had schizophrenia. I was too focused on getting out of residential treatment so I could return to my life — and the delusions I was experiencing.
+
+If I allowed my delusions to continue without seeking early intervention, I think it would’ve been even more difficult for me to process my internal trauma while receiving treatment. My delusions became my reality; it was all I knew. I created a new life for myself that I adapted to for months before I received help.
+
+I Got Wrapped Up In My Delusions
+Due to my illness, I had internalized the belief that I was the chosen one sent to save my basketball team from witchcraft attacks. Players on the team constantly suffered injuries on top of game losses. Their season was going downhill fast, and I believed that there must have been a witch behind the scenes casting spells on them. I knew I had to inform the public about what I was seeing behind the scenes.
+
+I was convinced God put me at the arena and made me a part of this on purpose. I had the “devil” on me, so I was not able to talk to the players until I fought him off. That looked like me rummaging around in my room, sometimes with the Bible in my hand, fighting off hallucinations I saw and three voices that once again included the “devil” himself.
+
+My paranoid thoughts didn’t end there. I constantly experienced the feeling that someone was touching me and breathing on my back. I was convinced that the devil and an ex-coworker of mine were sent to stalk me so I couldn’t reach the team. This delusion, which involved a huge invasion of my privacy, impacted my life greatly; I showered in a night gown and was constantly on guard, fearing they would harm me. I wasn’t going to let them win.
+
+I believed every single delusion to the point where I completely lost touch with reality. I held on so long to the point where my brain couldn’t properly function anymore, and I blanked out. I had no connection to myself anymore. I couldn’t tell you who I was. Luckily, my crisis happened around my family. Everything came to a head at my auntie’s funeral; I was just existing. I had no recollection of where I was, who I was talking to or what I was doing. My family monitored me and drove me home from Atlanta shortly after the funeral. They involuntarily admitted me to our nearest psych ward.
+
+I Learned To Accept My New Reality And Embrace Treatment
+While I was in the residential treatment, I went days without knowing what happened to me. It took a week to open my eyes and face the reality of what really happened to me. At first, I thought this hospitalization was all a part of the power struggle going on in my mind — and I had to say a magic word or phrase to break free.
+
+Reality eventually kicked in when I was outside with the rest of the patients, and we lined up to songs we wanted to be played. The music reminded me of my old home in New York. It brought back memories and some nostalgia, and something clicked in me. I was really there; I wasn’t dreaming, and no one was coming to save me.
+
+The hardest part about having schizophrenia is getting off the merry-go-round. You have to plan ahead for possible negative thoughts and be prepared to manage those thoughts when they arise — you cast down your thoughts so you don’t get stuck repeating them. I’ve been in recovery for close to a month now, and I’ve been feeling like myself with medication and therapy. I know I’ll never be the same as I was before my illness, but I have faith that I’ll continue to get better. Psychosis does happen, but it’s not the end.</p>
+    </div>
+  )
+}
