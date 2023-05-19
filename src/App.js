2caf91d7139f5879a91dda5864f8782cd0b08035
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2023-05-19 14:15:43 +0530

    Revert "Quiz analysis feature"

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import NavBar from "./NavBar";
-import HealthBot from "./links/HealthBot";
+import NavBar from './NavBar';
+import HealthBot from './links/HealthBot';
 import { Route, Routes } from "react-router-dom";
-import About from "./links/About";
-import Blog from "./links/Blog";
-import Blog1 from "./links/Blog1";
-import Blog2 from "./links/Blog2";
-import Blog3 from "./links/Blog3";
-import Blog4 from "./links/Blog4";
-import Home from "./links/Home";
-import Login from "./links/Login";
-import Music from "./links/Music";
-import Dashboard from "./links/Dashboard";
-import Parent from "./links/Parent";
-import AnxietyTest from "./links/AnxietyTest";
-import DepressionTest from "./links/DepressionTest";
-import Faq from "./links/Faq";
+import About from './links/About';
+import Blog from './links/Blog';
+import Blog1 from './links/Blog1';
+import Blog2 from './links/Blog2';
+import Blog3 from './links/Blog3';
+import Blog4 from './links/Blog4';
+import Home from './links/Home';
+import Login from './links/Login';
+import Music from './links/Music';
+import Dashboard from './links/Dashboard';
+import Parent from './links/Parent';
+import AnxietyTest from './links/AnxietyTest';
+import DepressionTest from './links/DepressionTest';
+
 
 function App() {
+
+
   // if(user) navigate("/");
 
   return (
     <>
       <NavBar />
-      <div className="components">
+      <div className='components'>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<Blog />} />
           <Route path="/blog1" element={<Blog1 />} />
-          <Route path="/blog2" element={<Blog2 />}></Route>
-          <Route path="/blog3" element={<Blog3 />}></Route>
-          <Route path="/blog4" element={<Blog4 />}></Route>
-          <Route path="/anxietyTest" element={<AnxietyTest />} />
-          <Route path="/depressionTest" element={<DepressionTest />} />
-          <Route path="/safeplace" element={<Music />} />
-          <Route path="/healthbot" element={<HealthBot />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/parent" element={<Parent />} />
-          <Route path="/HealthBot" element={<HealthBot />} />
-          <Route path="/support" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/blog2' element={<Blog2/>}></Route>
+          <Route path='/blog3' element={<Blog3/>}></Route>
+          <Route path='/blog4' element={<Blog4/>}></Route>
+          <Route path='/anxietyTest' element={<AnxietyTest/>}/>
+          <Route path='/depressionTest' element={<DepressionTest />} />
+          <Route path='/safeplace' element={<Music />} />
+          <Route path='/healthbot' element={<HealthBot/>}/>
+          <Route path='/faq' />
+          <Route path='/parent' element={<Parent />} />
+          <Route path='/HealthBot'  element={<HealthBot/>}/>
+          <Route path='/support' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<About/>}/>
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
       </div>
+
     </>
   );
 }
--- src/links/About.js
+++ src/links/About.js
@@ -1,61 +1,50 @@
-import React from "react";
+import React from 'react'
 import "../styles/about.css";
 
 export default function About() {
   return (
-    <>
-      <div class="row">
-        <div class="column">
-          <div class="card">
-            <img src="img1.jpg" />
-            <div class="container">
-              <h2>Dr. Ashish Sabharwal</h2>
-              <p class="title1">Bc.S., Psychologist </p>
-              {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+    <>    
+    <div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="img1.jpg" />
+      <div class="container">
+        <h2>Dr. Ashish Sabharwal</h2>
+        <p class="title1">Bc.S., Psychologist  </p>
+        {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>example@example.com</p> */}
-              <p>
-                <a href="https://findahelpline.com/in">
-                  <button class="button">Contact</button>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <img src="img2.jpg" />
-            <div class="container">
-              <h2>Dr. Sudhansu Bhattacharyya</h2>
-              <p class="title1">Psychologist</p>
-              {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p> */}
-              <p>
-                <a href="https://findahelpline.com/in">
-                  <button class="button">Contact</button>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <img src="img3.jpg" />
-            <div class="container">
-              <h2>Dr. Aditya Gupta</h2>
-              <p class="title1">Bc.S., Psychologist </p>
-              {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p> */}
-              <p>
-                <a href="https://findahelpline.com/in">
-                  <button class="button">Contact</button>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
+        <p><button class="button">Contact</button></p>
       </div>
-    </>
-  );
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="img2.jpg" />
+      <div class="container">
+        <h2>Dr. Sudhansu Bhattacharyya</h2>
+        <p class="title1">Psychologist</p>
+        {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p> */}
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="img3.jpg" />
+      <div class="container">
+        <h2>Dr. Aditya Gupta</h2>
+        <p class="title1">Bc.S., Psychologist </p>
+        {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p> */}
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div> 
+  </>
+
+  )
 }
